0d73586541c0770b8ad2383fe9e7c2db3e28d4f1
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-03 00:05:45 +0700

    Update whatsyourname.py

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,4 @@
+#!/home/swichaid/Desktop/python3/.venv/bin/python3
 ask = input("hey, what's your first name?: ")
 ask2 = input("what's your last name?: ")
 print('Well,please to meet you,',ask,ask2)

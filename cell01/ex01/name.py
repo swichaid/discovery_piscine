514904fc72891ef08740fb4f8a7d687d1d6de23f
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-02 14:16:30 +0700

    Update name.py

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-first_name = "Settthawut"
+first_name = "Setthawut"
 last_name = "Wichaidit"
-whole_name = (first_name,last_name)
-print(whole_name)
+whole_name = (first_name)+ " " +(last_name)
+print (whole_name)

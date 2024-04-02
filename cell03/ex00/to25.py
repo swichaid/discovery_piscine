da36a8581bbb7ded535b7013d70436e0c5eccd6e
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-02 22:42:18 +0700

    Update to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,1 +1,8 @@
-
+a = int(input('Enter number less than:\n '))
+def table():
+        if a <= 25 :
+            for i in range (a,26):
+                print(f'inside the loop, my variable is {i}')
+        else:
+            print("Error")
+table()

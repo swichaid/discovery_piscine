1f1b3b7b77b12d2b4df96cee1778ef19acd80448
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-03 01:32:35 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,1 +1,9 @@
-
+#!/home/swichaid/Desktop/python3/.venv/bin/python3
+msg1 = input('What you gotta say: ')
+if msg1 == 'STOP':
+   exit()
+else:
+    while True:
+        msg2= input('I got that! Any thing else: ')
+        if msg2 == 'STOP':
+            break

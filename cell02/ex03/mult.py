cc181f7d59af1b19b3084070529f0dbeb7c7ea7a
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-02 22:58:49 +0700

    Update and rename mul.py to mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    result = a * b
+    if result == 0:
+        print('the result is negative and positive.')
+    elif result < 0 :
+        print('the result is negative.')
+    else:
+        print('the result is positive.')
+except ValueError:
+    print('please enter the number')

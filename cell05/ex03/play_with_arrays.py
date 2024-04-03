114f93e5bc9632448cf694618d519bc0870763bc
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-04 00:13:50 +0700

    Create play_with_arrays.py

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+print(Original_array)
+New_array = [num + 2 for num in Original_array]
+New_array_filtered = [num for num in New_array if num >= 5]
+thissett = set(New_array_filtered)
+print(thissett)

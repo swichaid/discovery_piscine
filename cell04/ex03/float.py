bdc68c61984e96796f13e19acb720d46a07a0939
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-03 21:21:16 +0700

    Update float.py

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,1 +1,13 @@
+#!/home/swichaid/Desktop/python3/.venv/bin/python3
+try:
+    def main():
+        x = float(input("Give me a number : "))
+        if int(x) == x :
+                print("This number is integer.")
+        else:
+                print("This number is decimal.")
+except: print("please enter the number")
+
+main()
+
 

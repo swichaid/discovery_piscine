93479c96d08338a3a8b14b33c5f79f9843114a9e
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-03 23:44:57 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,1 +1,5 @@
-
+#!/usr/bin/python3
+Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+New_array = [num + 2 for num in Original_array]
+print("Original array:", Original_array)
+print("New array:", New_array)

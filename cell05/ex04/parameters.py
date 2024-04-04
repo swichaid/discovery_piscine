9b5bc43a2a391f71cd9c68bb6a647ddd7684c13b
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-04 12:06:18 +0700

    Update parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+import sys
+def main():
+    if len(sys.argv) <1:
+            print("Number of parameter:0")
+    else:
+            print("Number of parameter: ", len(sys.argv))
+main()

77ae4f34ae62f010f702b522667ab1787e947a26
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-04 22:15:12 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/python3
+import sys
+param = sys.argv[1:]
+    
+if not param:
+        print("none")
+else:
+    print("parameters:", len(param))
+    for i in param:
+        print(i, len(i))

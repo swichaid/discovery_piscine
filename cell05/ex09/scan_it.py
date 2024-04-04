b99f810795044637d0ebb8b2cf91655c2aef3fd1
swichaid <165653458+swichaid@users.noreply.github.com>
2024-04-04 19:06:19 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-key = sys.argv[1]
-target = sys.argv[2]
+
 if len(sys.argv) == 3:
+    key = sys.argv[1]
+    target = sys.argv[2]
     print(target.count(key))
 else:
     print("none")
